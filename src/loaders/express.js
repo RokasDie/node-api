895a06b274b06d7f999950800ba985c81181dd19
Rokas Diedonis <44611740+RokasDie@users.noreply.github.com>
2020-03-17 22:09:19 +0200

    Included error handlers

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -13,5 +13,47 @@ module.exports = ({ app }) => {
         res.status(200).end()
     })
 
-    app.use('/api', routes())
+    // The magic package that prevents frontend developers going nuts
+    // Alternate description:
+    // Enable Cross Origin Resource Sharing to all origins by default
+    app.use(cors())
+    // Load API routes
+    app.use(config.api.prefix, routes())
+
+    /// catch 404 and forward to error handler
+    app.use((req, res, next) => {
+        const err = new Error('Not Found')
+        err['status'] = 404
+        next(err)
+    })
+    
+    /// catch 404 and forward to error handler
+    app.use((req, res, next) => {
+        const err = new Error('Not Found')
+        err['status'] = 404
+        next(err)
+    })
+
+    /// error handlers
+    app.use((err, req, res, next) => {
+        /**
+         * Handle 401 thrown by express-jwt library
+         */
+        if (err.name === 'UnauthorizedError') {
+            return res
+                .status(err.status)
+                .send({ message: err.message })
+                .end()
+        }
+        return next(err)
+    })
+
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500)
+        res.json({
+            errors: {
+                message: err.message,
+            },
+        })
+    })
 }
